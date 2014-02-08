f56385355140e71a461a22a31aeec94ffd0d7d98
sarace <nomade08@gmail.com>
2014-02-08 11:10:21 -0800

    Create, read, edit and delete Academic Year

--- fishe-web/src/main/resources/org/fishe/db/mysql.sql
+++ fishe-web/src/main/resources/org/fishe/db/mysql.sql
@@ -8,3 +8,13 @@ create table user (
     last_name   varchar(50)  not null,
     email       varchar(100) not null
 ) engine = innodb;
+
+
+--changeset sarace:2
+
+create table academic_year (
+    id                              integer     not null primary key auto_increment,
+    academic_year                   integer     not null unique,
+    registration_start_date         date,
+    registration_end_date           date
+) engine = innodb;
