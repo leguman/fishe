58e05960558c4fd6e57de55b8e6047c0febb2a2e
bosman <bosman@Bosman-Portable.sips.ucl.ac.be>
2014-02-08 12:09:16 +0100

    TP vbosman

--- fishe-web/src/main/resources/org/fishe/db/mysql.sql
+++ fishe-web/src/main/resources/org/fishe/db/mysql.sql
@@ -8,3 +8,14 @@ create table user (
     last_name   varchar(50)  not null,
     email       varchar(100) not null
 ) engine = innodb;
+
+--changeset vbosman:2
+
+create table person (
+    id          Integer      not null primary key auto_increment,
+    first_name  Varchar(50)  not null,
+    last_name   Varchar(50)  not null,
+    birth_date   Date not null
+) engine = innodb;
+
+
