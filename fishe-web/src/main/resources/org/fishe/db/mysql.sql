3c3c39d50eb74ab6a7f6999ed2318fa7104f6ee3
Anthony <antho.tu@gmail.com>
2014-02-10 09:33:54 +0100

    Fixed #5

--- fishe-web/src/main/resources/org/fishe/db/mysql.sql
+++ fishe-web/src/main/resources/org/fishe/db/mysql.sql
@@ -8,3 +8,10 @@ create table user (
     last_name   varchar(50)  not null,
     email       varchar(100) not null
 ) engine = innodb;
+
+--changeset leguman:2
+create table course (
+    id          integer     not null primary key auto_increment,
+    name        varchar(50) not null,
+    description varchar(50) not null
+) engine = innodb;
