bd3890e6698db30489da92950f5ac6039e22b610
unknown <verpoorten@PC-220-968.oasis.uclouvain.be>
2014-02-14 09:47:20 +0100

    Conflicts solved.µ

---
+++ fishe-web/src/main/java/org/fishe/web/institution/controller/PersonMBean.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.fishe.web.institution.controller;
+
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.RequestScoped;
+import org.fishe.institution.business.PersonBean;
+import org.fishe.institution.domain.Person;
+import org.fishe.institution.domain.User;
+
+
+/**
+ *
+ * @author bosman
+ */
+@ManagedBean
+@RequestScoped
+public class PersonMBean {
+
+    /**
+     * Creates a new instance of PersonMBean
+     */
+   @EJB
+   private PersonBean personBean;
+    private List<Person> people;
+
+    @ManagedProperty(value="#{param.id}")
+    private Integer id;
+
+    private Person person;
+
+    public List<Person> getPeople() {
+        if(people == null) {
+            people = personBean.findAll();
+        }
+        return people;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Person getPerson() {
+        return this.person;
+    }
+
+    @PostConstruct
+    public void load() {
+        if(id != null) {
+            this.person = personBean.find(id);
+        }
+        else {
+            this.person = personBean.create();
+        }
+    }
+
+    public String save() {
+        personBean.save(this.person);
+        return "people";
+    }
+
+    public String remove() {
+        personBean.remove(this.person.getId());
+        return "people";
+    }
+    
+}
